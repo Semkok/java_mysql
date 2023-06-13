afe1aa82bdb872a1a2518a88b951ab19d6967b1f
SemKok <semster0509@gmail.com>
2023-06-13 15:14:55 +0200

    made more variables

--- src/SendQuery.java
+++ src/SendQuery.java
@@ -1,14 +1,14 @@
 import java.sql.*;
 public class SendQuery {
 	
-	//column names
+	//SQL table information 
 	final static private String databaseName = "xtapp";
 	final static private String table_id = "id";
 	final static private String column_user_name = "user_name";
 	final static private String column_score = "score";
 	final static private String column_performance_points =  "performance_points"; 
-	//connection 
-	final static private String connection = "jdbc:mysql://localhost:3306/xtapp?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+	//connection information
+	final static private String connection = "jdbc:mysql://localhost:3306/" + databaseName +"?allowPublicKeyRetrieval=false&useSSL=false&serverTimezone=UTC";
 	final static private String user = "root";
 	final static private String password = "";
 	
@@ -30,10 +30,10 @@ public class SendQuery {
 		         
 		         
 		         while(rset.next()) {   
-		        	int id = rset.getInt("id");
-		            String user_name = rset.getString("user_name");  
-		            int score = rset.getInt("score");  
-		            int    performance_points   = rset.getInt("performance_points");       
+		        	int id = rset.getInt(table_id);
+		            String user_name = rset.getString(column_user_name);  
+		            int score = rset.getInt(column_score);  
+		            int    performance_points   = rset.getInt(column_performance_points);       
 		            System.out.printf("%d %s %d %d \n", id ,user_name, score, performance_points);
 		         }
 		         
@@ -48,13 +48,10 @@ public class SendQuery {
 	static void insertQuery(int id,String playername,int score,int performance_points) {
 		// id, playername,score,performance_points
 		try (
-		       
 				Connection conn = DriverManager.getConnection(connection,user,password);    
 		         
 		         Statement stmt = conn.createStatement();
 		      ) {
-		         
-			
 		         String sqlInsert = String.format("INSERT INTO highscores values (%d,'%s',%d,%d)",id,playername,score,performance_points);
 		         
 		         stmt.executeUpdate(sqlInsert);
