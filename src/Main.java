790cf948c6d0c97352b5b38421b7580ebf11f001
SemKok <semster0509@gmail.com>
2023-06-27 09:46:41 +0200

    Array list gemaakt zodat we makkelijker de highscores kunnen ophalen

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		SendQuery a = new SendQuery(8,"henk",223,22);
+		SendQuery a = new SendQuery(13,"h",223,22);
 		
 		
 		
--- src/SendQuery.java
+++ src/SendQuery.java
@@ -3,6 +3,7 @@ import java.sql.Statement;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class SendQuery {
 	
@@ -27,8 +28,10 @@ public class SendQuery {
 	
 	
 	
-	 private String readDatabase() {
+	 private ArrayList<String> readDatabase() {
 		
+		 // een arraylist
+		 ArrayList<String> info = new ArrayList<String>();
 		
 		try (
 				// maakt een connectie met de database
@@ -50,70 +53,78 @@ public class SendQuery {
 		            String user_name = rset.getString(column_user_name);  
 		            int score = rset.getInt(column_score);  
 		            int    performance_points   = rset.getInt(column_performance_points);       
-		            System.out.println(user_name);
+		         
 		            
-		            // moet nog meer returnen!!!
-		            return table_id + " " + id + "\n" + column_user_name + " "  + user_name + "\n" + column_score + " " +  score + "\n" + column_performance_points + " " + performance_points + "\n\n";
+		            String row = String.format("%s %d \n %s %s \n %s %d \n %s %d \n \n",table_id, id, column_user_name, user_name, column_score, score, column_performance_points, performance_points);
+		            info.add(row);
+		            
 		         }
+		         System.out.println(info);
+		         
+		         return info;
 		         
 		      } catch(SQLException e) {
 		         e.printStackTrace();
 		      }
 		return null;
+		
 	
 	}
 	
 	
 	
 	private void insertQuery(int id,String playername,int score,int performance_points) {
+		readDatabase();
+		
+		
 		// id, playername,score,performance_points
-		try (
-				Connection conn = DriverManager.getConnection(connection,user,password);    
-		         
-		         Statement stmt = conn.createStatement();
-				
-				
-				
-		      ) {
-			
-				// alle gegevens van de database in een string
-			    String info = readDatabase();
-			    
-			    System.out.println(info);
-			    
-			    // of de spelernaam er al in zit
-			    boolean playerIsIncluded = info.contains(playername);
-			    
-			    
-			    //  met id 
-			    boolean idIsIncluded = info.contains(Integer.toString(id));
-			    
-			    // als de speler er al in zit in de highscoretable
-			    if(playerIsIncluded == true) {
-			    	// haalt de speler weg die er al in zit
-			    	String sqlDelete = String.format("DELETE FROM %s WHERE %s = '%s' OR %s = '%s'",table_name,column_user_name,playername,table_id,id);
-			         
-			         stmt.executeUpdate(sqlDelete);
-			       
-			    	
-			         String sqlInsert = String.format("INSERT INTO highscores values (%d,'%s',%d,%d)",id,playername,score,performance_points);
-    		         
-			         stmt.executeUpdate(sqlInsert);
-			    	
-			    	
-			    }
-			    // laat de query door gaan en zorgt dat de nieuwe speler in de highscore table komt 
-			    else {
-			    	String sqlInsert = String.format("INSERT INTO highscores values (%d,'%s',%d,%d)",id,playername,score,performance_points);
-			         stmt.executeUpdate(sqlInsert);
-			    }
-			     
-			     
-			
-		         
-		        
-		      } catch(SQLException ex) {
-		         ex.printStackTrace();
-		      } 
+//		try (
+//				Connection conn = DriverManager.getConnection(connection,user,password);    
+//		         
+//		         Statement stmt = conn.createStatement();
+//				
+//				
+//				
+//		      ) {
+//			
+//				// alle gegevens van de database in een string
+//			    String info = readDatabase();
+//			    
+//			    System.out.println(info);
+//			    
+//			    // of de spelernaam er al in zit
+//			    boolean playerIsIncluded = info.contains(playername);
+//			    
+//			    
+//			    //  met id 
+//			    boolean idIsIncluded = info.contains(Integer.toString(id));
+//			    
+//			    // als de speler er al in zit in de highscoretable
+//			    if(playerIsIncluded == true) {
+//			    	// haalt de speler weg die er al in zit
+//			    	String sqlDelete = String.format("DELETE FROM %s WHERE %s = '%s' OR %s = '%s'",table_name,column_user_name,playername,table_id,id);
+//			         
+//			         stmt.executeUpdate(sqlDelete);
+//			       
+//			    	
+//			         String sqlInsert = String.format("INSERT INTO highscores values (%d,'%s',%d,%d)",id,playername,score,performance_points);
+//    		         
+//			         stmt.executeUpdate(sqlInsert);
+//			    	
+//			    	
+//			    }
+//			    // laat de query door gaan en zorgt dat de nieuwe speler in de highscore table komt 
+//			    else {
+//			    	String sqlInsert = String.format("INSERT INTO highscores values (%d,'%s',%d,%d)",id,playername,score,performance_points);
+//			         stmt.executeUpdate(sqlInsert);
+//			    }
+//			     
+//			     
+//			
+//		         
+//		        
+//		      } catch(SQLException ex) {
+//		         ex.printStackTrace();
+//		      } 
 	}
 }
